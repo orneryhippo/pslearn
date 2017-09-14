6f04d910ff1a4d12ce6cb0319d0b6889a552a176
Jesse <jcassill@bofifederalbank.com>
2017-09-14 11:38:12 -0700

    Adding twilio module

---
+++ lib/servy/calltwilio.ex
@@ -0,0 +1,11 @@
+defmodule Servy.CallTwilio do
+	@acct_sid ""
+	@test_sid "AC0525379676faeb5d0fe9e5cb9713854d"
+	@test_auth "0d08cee777d08713948656847892766a"
+
+	def show() do
+		IO.puts "#{test_sid}"
+		IO.puts "#{test_auth}"
+
+	end
+end
