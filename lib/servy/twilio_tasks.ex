b5f04b4c3ffaf5a1b917a7d763a007ecb0407488
Jesse <jcassill@bofifederalbank.com>
2017-09-14 15:19:14 -0700

    Adding twilio module

---
+++ lib/servy/twilio_tasks.ex
@@ -0,0 +1,46 @@
+defmodule Servy.TwilioTasks do
+	@moduledoc """
+	handles asynchronous dispatch to Twilio
+	"""
+	def twilio_url() do
+		#this needs to be a configuration set item
+		#for now it is hardcoded to a fake address here
+		"https://damp-journey-48506.herokuapp.com"
+	end
+
+	defp std_headers() do
+		# nope: headers = %{ 'content-type' => "application/x-www-form-urlencoded", 'cache-control' => "no-cache" }
+		headers = [{"content-type", "x-www-form-urlencoded"}, {'cache-control' , "no-cache"}]
+	end
+
+	defp call_req(payload, url) do
+		headers = std_headers()
+		IO.puts url
+		IO.inspect payload
+		IO.inspect headers
+		#response = HTTPoison.put(url, payload, headers)
+		response = HTTPoison.request(:post, url, body, headers )
+		process_response(response)
+		response
+	end
+
+	def process_response(response) do
+		#debugging this new function
+		IO.inspect response
+	end
+
+	def call_twilio(the_digits \\ "000000000") do
+		top_num = "+18582631960"
+		from = "+14012503981"
+		twilio_url = Path.join(twilio_url(), "Calls.json")
+		app_url = "https://damp-journey-48506.herokuapp.com"
+		callback_url = Path.join(Path.join(app_url, "connected"), the_digits)
+		#payload = %{'To' => top_num, 'From' => from, 'Url' => callback_url, 'Method' => 'POST', 'SendDigits' => 'www1www1www', 'Timeout' => 20}
+
+		body = "{:form [{'To', '#{ top_num }'}, {'From', '#{ from }'}, {'Url', '#{ callback_url }'}, {'Method', 'POST'}, {'SendDigits', 'www1www1www'}, {'Timeout', 20, ]}"
+		IO.puts body
+		IO.puts twilio_url
+		#call_req(payload, twilio_url)
+	end
+
+end
