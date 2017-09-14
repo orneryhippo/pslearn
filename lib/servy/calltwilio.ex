da64e586e3571e8dc4f3bc86447a80b477399379
Jesse <jcassill@bofifederalbank.com>
2017-09-14 11:47:08 -0700

    Adding twilio module

--- lib/servy/calltwilio.ex
+++ lib/servy/calltwilio.ex
@@ -1,13 +1,8 @@
 defmodule Servy.CallTwilio do
 	def twilio_url() do
-		_acct = "ACea64eff94999fd45f71ed6a397cf4251"
-		_auth = "3bf3e6189776034376e8fbbbbda349a5"	
-		"https://#{_acct}:#{_auth}@api.twilio.com/2010-04-01/Accounts/#{_acct}" 
+		acct = "ACea64eff94999fd45f71ed6a397cf4251"
+		auth = "3bf3e6189776034376e8fbbbbda349a5"	
+		"https://#{acct}:#{auth}@api.twilio.com/2010-04-01/Accounts/#{acct}" 
 	end
 
-	def show() do
-		IO.puts "#{test_sid}"
-		IO.puts "#{test_auth}"
-
-	end
 end
