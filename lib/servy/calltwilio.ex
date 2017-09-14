01954e89b8b3ff399fa0e82c92ea176b746c1d7b
Jesse <jcassill@bofifederalbank.com>
2017-09-14 13:24:17 -0700

    Adding twilio module

--- lib/servy/calltwilio.ex
+++ lib/servy/calltwilio.ex
@@ -1,10 +1,5 @@
 defmodule Servy.CallTwilio do
 
-	def twilio_url() do
-		acct = "ACea64eff94999fd45f71ed6a397cf4251"
-		auth = "3bf3e6189776034376e8fbbbbda349a5"	
-		"https://#{acct}:#{auth}@api.twilio.com/2010-04-01/Accounts/#{acct}" 
-	end
 
 	def empty_response() do
 		'<?xml version="1.0" encoding="UTF-8"?><Response></Response>'
@@ -12,7 +7,7 @@ defmodule Servy.CallTwilio do
 
 	def std_headers() do
 		headers = %{ 'content-type' => "application/x-www-form-urlencoded", 'cache-control' => "no-cache" }
-		headers
+		headers = [{"content-type", "x-www-form-urlencoded"}, {'cache-control' , "no-cache"}]
 	end
 
 	def call_req(payload, url) do
