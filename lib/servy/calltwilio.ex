24d5f211322991ba0921baae19c3cf82b724da18
Jesse <jcassill@bofifederalbank.com>
2017-09-14 15:29:45 -0700

    Adding twilio module

--- lib/servy/calltwilio.ex
+++ lib/servy/calltwilio.ex
@@ -3,7 +3,7 @@ defmodule Servy.CallTwilio do
 	import Servy.TwilioTasks
 
 	def request_info(the_digits) do
-		task = Task.async__MODULE__, &call_twilio/1, [the_digits])
+		task = Task.async(__MODULE__, &call_twilio/1, [the_digits])
 		IO.inspect task
 	end
 
